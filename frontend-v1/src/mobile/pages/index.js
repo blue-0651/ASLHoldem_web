ddaadcb097de2b62f908f55ea6841032a8f6de0f
yhkim <yk110796@gmail.com>
2025-06-22 16:31:14 +0900

    인덱스 페이지 .js 빠진 오류 수정

--- frontend-v1/src/mobile/pages/index.js
+++ frontend-v1/src/mobile/pages/index.js
@@ -1,10 +1,10 @@
 // 모바일 페이지 컴포넌트 내보내기
-import StoreDashboard from './store/StoreDashboard';
-import StoreInfo from './store/StoreInfo';
-import Tournament from './store/Tournament';
-import PlayerRegistration from './store/PlayerRegistration';
-import TicketIssue from './store/TicketIssue';
-import UserDashboard from './user/UserDashboard';
+import StoreDashboard from './store/StoreDashboard.jsx';
+import StoreInfo from './store/StoreInfo.jsx';
+import Tournament from './store/Tournament.jsx';
+import PlayerRegistration from './store/PlayerRegistration.jsx';
+import TicketIssue from './store/TicketIssue.jsx';
+import UserDashboard from './user/UserDashboard.jsx';
 
 // 컴포넌트 내보내기
 export {
@@ -17,7 +17,7 @@ export {
 };
 
 // Common (공통) 페이지
-export { default as MobileLogin } from './common/MobileLogin';
-export { default as MobileSignup } from './common/MobileSignup';
-export { default as Settings } from './common/Settings';
-export { default as UnderConstruction } from './common/UnderConstruction'; 
+export { default as MobileLogin } from './common/MobileLogin.jsx';
+export { default as MobileSignup } from './common/MobileSignup.jsx';
+export { default as Settings } from './common/Settings.jsx';
+export { default as UnderConstruction } from './common/UnderConstruction.jsx'; 
